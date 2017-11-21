7a97b2d03161f410a5cd831fa8f7422bf03d2600
walaamedhat <nicedree@gmail.com>
2017-11-21 14:57:28 +0200

    update2

--- JS/logic.js
+++ JS/logic.js
@@ -10,6 +10,7 @@ function fetch (url, callback) {
   xhr.addEventListener('load', function () {
     if (xhr.status === 200) {
       var response = JSON.parse(xhr.responseText);
+      callback(response)
     }
     else {
       console.log('Status Code: ' + xhr.status);
@@ -20,20 +21,38 @@ function fetch (url, callback) {
 }
 
 addListener('#submitButton','submit', function (event) {
-  event.preventDefault() ; 
+  event.preventDefault() ;
 
   var endpoint = "http://api.musixmatch.com/ws/1.1/track.search?q_artist=";
   var form = event.target;
-  // console.log(form);
   var singer = form.querySelector('input[name=name]').value;
-  // console.log(singer);
   var apiKey = "&apikey=547631ad01eec50f795c47471f724f80" ;
   var url =  endpoint + singer + apiKey ;
   // console.log(url) ;
   //http://api.musixmatch.com/ws/1.1/track.search?q_artist=selena&apikey=547631ad01eec50f795c47471f724f80
 // 5
-  fetch(url, function (response) {//8 response
-      // ... do something with the response
-        console.log(url) ;
-  });
+  fetch(url, function (response) {
+    var msg= response.message.body.track_list;
+    console.log(msg);
+    // var arr =[];
+    // for (var i in msg){
+    //   arr.push(msg[i].track.track_name);
+    //   }
+    var songs = msg.map(function(x){
+      return  x.track.track_name ;
+    });
+    console.log(songs);
+    
+    for (var i = 0; i < songs.length; i++) {
+      var ul=document.getElementById('song');
+      var li=document.createElement('li');
+      var a=document.createElement('a');
+      var song=songs[i];
+      var songname = document.createTextNode(song);
+      a.appendChild(songname);
+      li.appendChild(a);
+      ul.appendChild(li);
+        }
+
+    });
 });
