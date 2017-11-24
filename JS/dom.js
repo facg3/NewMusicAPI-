0e2729beb8d2625fb7d832afc44db6d76ec0433a
walaamedhat <nicedree@gmail.com>
2017-11-24 06:30:32 +0200

    fix error

--- JS/dom.js
+++ JS/dom.js
@@ -1,4 +1,3 @@
-
 addListener('#submitForm', 'submit', function(event) {
   event.preventDefault();
   var endpoint = "https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_artist=";
@@ -6,15 +5,11 @@ addListener('#submitForm', 'submit', function(event) {
   var singer = form.querySelector('input[name=name]').value;
   var s =singer;
   singer = singer.replace(/ /g, '%20');
-  var form = event.target;
   var apiKey = "&apikey=547631ad01eec50f795c47471f724f80";
   var url = endpoint + singer + apiKey+"&s_track_rating=desc";
   fetch(url, function(response) {
     document.getElementById("song").innerHTML = "";
     if (singer.trim() != "") {
-
-
-
       document.getElementsByClassName("number")[0].innerHTML = "Top 10  songs for \"" + s + "\"";
 
       var msg = response.message.body.track_list;
@@ -25,8 +20,8 @@ addListener('#submitForm', 'submit', function(event) {
         var ul = document.getElementById('song');
         var li = document.createElement('li');
         var a = document.createElement('a');
-        linksyoutube(element, singer, function(msg) {
-          a.setAttribute("href", msg);
+          linksyoutube(element, singer, function(msg) {
+            a.setAttribute("href", msg);
           a.setAttribute("target","blank")
         });
         var songname = document.createTextNode(element);
